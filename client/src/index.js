0bd67b26dafcd5d9198958ca93f05512ec3ebee9
Martin <martin.musinn@gmail.com>
2022-05-09 17:14:21 +0300

    smth resolved

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import {BrowserRouter} from 'react-router-dom';
-import { store } from './redux';
+import { store } from '../src/redux';
 import { Provider } from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
