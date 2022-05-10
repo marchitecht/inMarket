a4438e2d29d9f7c5004a1d35de776fca772d4234
Svetik <svetlana_lvova_info@mail.ru>
2022-05-10 19:57:05 +0300

    SubCategory done2

--- client/src/components/ProductsExemple/Test.jsx
+++ client/src/components/ProductsExemple/Test.jsx
@@ -16,12 +16,12 @@ const products = [{
   img: 'https://pro-dachnikov.com/uploads/posts/2021-11/1637956649_12-pro-dachnikov-com-p-tikva-foto-13.jpg',
   price: 1000,
   desc: 'good',
-  userImg: 'https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp',
+  userImg: 'http://www.almazfea.com/upload/items/417.jpg',
 },
 {
   name: 'bakinskie pomidori',
   link: '/',
-  img: 'https://delimanoua.azureedge.net/media/article/image/cache/580x436/a/-/a-28430_1.jpg',
+  img: 'https://images11.domashnyochag.ru/upload/img_cache/0de/0de365b0d0ee065e55522f4aa5ce7f5c_cropped_1332x888.jpg',
   price: 1000,
   desc: 'good',
   userImg: 'https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp',
@@ -29,7 +29,7 @@ const products = [{
 {
   name: 'bakinskie pomidori',
   link: '/',
-  img: 'https://delimanoua.azureedge.net/media/article/image/cache/580x436/a/-/a-28430_1.jpg',
+  img: 'https://dolgo-jv.ru/wp-content/uploads/2018/06/Taste-and-smell_web2.jpg',
   price: 1000,
   desc: 'good',
   userImg: 'https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp',
@@ -69,32 +69,18 @@ const products = [{
 
 function TestPage() {
 
-    const [imgType, setImgType] = useState(new Array(products.length).fill());
-    const [avatarType, setAvatarType] = useState(new Array(products.length).fill());
+    const [imgType, setImgType] = useState({});
+    const [avatarType, setAvatarType] = useState({});
 
     const onImgLoad = ({ target: img }, index) => {
       const { offsetHeight, offsetWidth } = img;
-      setImgType(imgType.map((el, i) => {
-        if (index === i) {
-          return offsetHeight >= offsetWidth ? 'h' : 'w'
-        }
-        return el;
-      }));
+      setImgType(prev => ({...prev, [index]: offsetHeight >= offsetWidth ? 'h' : 'w'}));
     };
 
     const onAvatarLoad = ({ target: img }, index) => {
       const { offsetHeight, offsetWidth } = img;
-      setAvatarType(avatarType.map((el, i) => {
-        if (index === i) {
-          return offsetHeight >= offsetWidth ? 'h' : 'w'
-        }
-        return el;
-      }));
+      setAvatarType(prev => ({...prev, [index]: offsetHeight >= offsetWidth ? 'h' : 'w'}));
     };
-
-    useEffect(() => {
-      
-    }, [imgType]);
   
   return (
     <div className="card">
